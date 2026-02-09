77ed85ef02c54bf9e13287831d08823536e0bf50
Pablo Serrano García <disciplineart17@gmail.com>
2026-02-09 13:04:49 +0000

    Lleno de errores

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,155 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const API_URL = "https://playground.4geeks.com/todo"
+  const USER = "Troglodita"
+ 
+  const [tarea, setTarea] = useState ("")
+  const [lista, setLista] = useState ([])
+
+
+  console.log(lista);
+
+
+  const handleInputChange = (e) => {
+
+
+    console.log(e);
+   
+
+
+    setLista({
+      ...lista,
+      [e.target.name]: e.target.value
+    })
+  }
+
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+
+
+  if (!lista) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "No tienes nada en la lista",
+    });
+    return;
+  }
+
+
+  createTarea();
 };
+ 
+
+
+  const getCharacter = async () => {
+
+
+    const response = await fetch(`${API_URL}/users/${USER}`)
+    console.log(response);
+
+
+    if (!response.ok) {
+      console.log("Debes crear un Usuario");
+      createUser ()
+      return
+     
+    }
+   
+    const data = await response.json()
+    console.log(data);
+    setTarea(data)
+   
+  }
+
+
+    const createUser = async () => {
+
+
+    const response = await fetch(`${API_URL}/users/${USER}`,{
+      method: "POST"
+    })
+    console.log(response);
+
+
+    if (!response.ok) {
+      console.log("Debes crear un Usuario");
+      return
+    }
+    const data = await response.json()
+    console.log(data);
+  }
+
+
+const createTarea = async () => {
+  const nuevaTarea = {
+    label: lista,
+    is_done: false
+  };
+
+
+  const response = await fetch(`${API_URL}/todos/${USER}`, {
+    method: "POST",
+    body: JSON.stringify(nuevaTarea),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+
+  const data = await response.json();
+  console.log(data);
+};
+ 
+
+
+
+
+  useEffect (() => {
+
+
+    getCharacter();
+
+
+  },[])
+
+
+  return (
+    <div className="d-flex flex-column align-items-center mt-5">
+      <h1 className="display-3">Lista de Tareas</h1>
+
+
+      <form  onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Escribe tu tarea"
+          value={lista}
+          onChange={handleInputChange}
+         
+        />
+
+
+        <ul>
+          {lista.map((item, index) => (
+            <li key={index}>
+              {item}
+            </li>
+          ))}
+        </ul>
+   
+      </form>
+
+
+ 
+    </div>
+  );
+};
+
 
 export default Home;
